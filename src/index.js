1c135365f0e9478e1633c0940c183e5b96030432
Leon <hualiang.tian@gmail.com>
2021-09-14 23:16:54 -0400

    commit tree fd6582dc72683686df23e4fe40a55199ec6f8d24

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
+
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => 
+{
+    fetchImages()
+    fetchBreeds()
+})
+
+//debugger
+function fetchImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(dogs => showImage(dogs["message"]))
+}
+
+function showImage(dogs){
+    const divImage = document.getElementById("dog-image-container")
+    dogs.forEach((image) => {
+        // divImage.innerHTML += `<img src=${image} />`
+        const img = document.createElement('img')
+        img.src = image
+        divImage.appendChild(img)
+    })
+
+}
+// ------------------------------------------------------------------------------------------------
+function fetchBreeds(){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(breeds => addBreeds(breeds["message"]))
+}
+
+function addBreeds(breeds){
+    //  console.log(breeds)
+    for(breed in breeds)
+    {
+        displayBreed(breed)
+    }
+
+    // debugger
+
+    function displayBreed(breed)
+    {
+        const breedsUl = document.getElementById('dog-breeds');
+        const breedsName = document.createElement('li');
+        breedsName.innerText = breed;
+        const dogType = document.createElement("ul");
+        breedsUl.appendChild(breedsName)
+        // debugger
+        for(type of breeds[breed])
+        {
+            const typeName = document.createElement("li");
+            typeName.innerText = type;
+            breedsName.appendChild(dogType)
+            dogType.appendChild(typeName);
+            // debugger
+
+        }
+    }
+
+
+}
+
